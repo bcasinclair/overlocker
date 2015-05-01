aee911c4be0e6f9e42825b4435c307b335552ac5
Andrew Sinclair <asinclair@brightcove.com>
2015-05-01 13:46:36 +1000

    Update location of encoded files

--- encoder.rb
+++ encoder.rb
@@ -8,7 +8,7 @@ class Encoder
         # Most basic version 1
         # Get the filename from the url
         uri = URI.parse(url)
-        filename = File.join('tmp',File.basename(uri.path))
+        filename = File.join('shared',File.basename(uri.path))
         frames_cmd = "ffmpeg -i #{url} -ss #{start} -t #{finish} -c:v libx264 -b:v #{rate}k -s #{width}x#{height} -pix_fmt yuv420p -y #{filename}_#{start}_#{finish}.ts"
         puts frames_cmd
         puts "Creating video part file"
